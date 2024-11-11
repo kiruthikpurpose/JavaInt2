5ca52024ff9d480069dda8b15d7be46f0decd6e3
KIRUTHIK M R <kiruthikpurpose@gmail.com>
2024-11-11 09:58:54 +0530

    Made few corrections

--- Day10-14/Day10/Main1.java
+++ Day10-14/Day10/Main1.java
@@ -2,6 +2,7 @@ public class Main1 {
     public static void main(String[] args) {
         try {
             int result = 10 / 0;
+            System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println("Caught an ArithmeticException: " + e.getMessage());
         }
